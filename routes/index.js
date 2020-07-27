c1deb23b34961e31e609689f494c195b46638b76
aslan <social.smur@simplelogin.fr>
2020-07-27 17:10:10 +0100

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: "Mini Messageboard", messages: messages });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'Form'});
+})
+
+router.use((req,res) => {
+  res.render('error')
+})
+
+router.post('/new', function(req, res, next) {
+  messages.push({text: req.body.text, user: req.body.user, added: new Date().toLocaleString()});
+  res.redirect('/')
+});
+
+module.exports = router;
